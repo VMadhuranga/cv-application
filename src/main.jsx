cf2de982dbfa70f114d3baa2956211a346575360
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-08 08:53:15 +0530

    Add CVInformation component to root

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { CVInformation } from "./components/cv-information/cv-information";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <CVInformation></CVInformation>
+  </React.StrictMode>,
 );

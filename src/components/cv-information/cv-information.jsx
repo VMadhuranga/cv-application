3d5559424e050548b78d809ed89489b5b255584d
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-08 10:57:21 +0530

    Add state to CVData object
    Add state to CVData object using useImmer and pass
    the state to GeneralInformation component using props

--- src/components/cv-information/cv-information.jsx
+++ src/components/cv-information/cv-information.jsx
@@ -2,12 +2,17 @@ import { CVData } from "../../cv-data";
 import { GeneralInformation } from "./general-information/general-information";
 import { Education } from "./education/education";
 import { WorkExperience } from "./work-experience/work-experience";
+import { useImmer } from "use-immer";
 
 export function CVInformation() {
+  const [cvData, updateCVData] = useImmer(CVData);
+
+  console.log(cvData.generalInformation);
   return (
     <form>
       <GeneralInformation
-        generalInformation={CVData.generalInformation}
+        generalInformation={cvData.generalInformation}
+        updateGeneralInformation={updateCVData}
       ></GeneralInformation>
       <Education educations={CVData.educations}></Education>
       <WorkExperience workExperiences={CVData.workExperiences}></WorkExperience>

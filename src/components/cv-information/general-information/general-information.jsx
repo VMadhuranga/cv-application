3bc7a053a742d430ce5ad9ee15604df26dd2d31b
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-08 10:58:09 +0530

    Create event handler functions
    Create event handler functions to change general information
    on cv data object

--- src/components/cv-information/general-information/general-information.jsx
+++ src/components/cv-information/general-information/general-information.jsx
@@ -1,6 +1,39 @@
 import { Input } from "../input";
 
-export function GeneralInformation({ generalInformation }) {
+export function GeneralInformation({
+  generalInformation,
+  updateGeneralInformation,
+}) {
+  function handleFirstNameChange(e) {
+    updateGeneralInformation((draft) => {
+      draft.generalInformation.firstName = e.target.value;
+    });
+  }
+
+  function handleLastNameChange(e) {
+    updateGeneralInformation((draft) => {
+      draft.generalInformation.lastName = e.target.value;
+    });
+  }
+
+  function handleAddressChange(e) {
+    updateGeneralInformation((draft) => {
+      draft.generalInformation.address = e.target.value;
+    });
+  }
+
+  function handleEmailChange(e) {
+    updateGeneralInformation((draft) => {
+      draft.generalInformation.email = e.target.value;
+    });
+  }
+
+  function handleTelephoneChange(e) {
+    updateGeneralInformation((draft) => {
+      draft.generalInformation.telephone = e.target.value;
+    });
+  }
+
   return (
     <section>
       <h2>General Information</h2>
@@ -9,30 +42,35 @@ export function GeneralInformation({ generalInformation }) {
         type={"text"}
         text={"First Name: "}
         value={generalInformation.firstName}
+        handleOnChange={handleFirstNameChange}
       ></Input>
       <Input
         id={"lastName"}
         type={"text"}
         text={"Last Name: "}
         value={generalInformation.lastName}
+        handleOnChange={handleLastNameChange}
       ></Input>
       <Input
         id={"address"}
         type={"text"}
         text={"Address: "}
         value={generalInformation.address}
+        handleOnChange={handleAddressChange}
       ></Input>
       <Input
         id={"email"}
         type={"email"}
         text={"Email: "}
         value={generalInformation.email}
+        handleOnChange={handleEmailChange}
       ></Input>
       <Input
         id={"telephone"}
         type={"tel"}
         text={"Telephone: "}
         value={generalInformation.telephone}
+        handleOnChange={handleTelephoneChange}
       ></Input>
     </section>
   );

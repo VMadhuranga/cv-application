c8fb4e20ab27c4b4f81a66db145f41fa826b6181
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-08 14:03:54 +0530

    Add event handler to text-area element from props

--- src/components/cv-information/text-area.jsx
+++ src/components/cv-information/text-area.jsx
@@ -1,8 +1,13 @@
-export function TextArea({ id, text, rows = 4, value }) {
+export function TextArea({ id, text, rows = 4, value, handleOnChange }) {
   return (
     <div>
       <label htmlFor={id}>{text}</label>
-      <textarea id={id} rows={rows} value={value}></textarea>
+      <textarea
+        id={id}
+        rows={rows}
+        value={value}
+        onChange={handleOnChange}
+      ></textarea>
     </div>
   );
 }

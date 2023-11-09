f373478b001fd332786e62c70d6d6b9792c145b1
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-09 14:54:59 +0530

    Add App component and remove CVInformation component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { CVInformation } from "./components/cv-information/cv-information";
+import { App } from "./components/app";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CVInformation></CVInformation>
+    <App></App>
   </React.StrictMode>,
 );

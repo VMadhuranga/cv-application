e7c78fed00f7d563fac91022a94aa68e6db6b5e3
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-09 08:02:02 +0530

    Add NewWorkExperience component to WorkExperience component

--- src/components/cv-information/work-experience/work-experience.jsx
+++ src/components/cv-information/work-experience/work-experience.jsx
@@ -1,43 +1,19 @@
-import { Input } from "../input";
-import { TextArea } from "../text-area";
+import { NewWorkExperience } from "./new-work-experience";
 
 export function WorkExperience({ workExperiences }) {
   const workExperiencesArr = [];
 
   workExperiences.forEach((experience) => {
     workExperiencesArr.push(
-      <div key={experience.id}>
-        <Input
-          id={`title${experience.id}`}
-          type={"text"}
-          text={"Title: "}
-          value={experience.title}
-        ></Input>
-        <Input
-          id={`companyName${experience.id}`}
-          type={"text"}
-          text={"Company Name: "}
-          value={experience.companyName}
-        ></Input>
-        <TextArea
-          id={`workDescription${experience.id}`}
-          type={"text"}
-          text={"Description: "}
-          value={experience.description}
-        ></TextArea>
-        <Input
-          id={`workStartDate${experience.id}`}
-          type={"text"}
-          text={"Start Date: "}
-          value={experience.startDate}
-        ></Input>
-        <Input
-          id={`workEndDate${experience.id}`}
-          type={"text"}
-          text={"End Date: "}
-          value={experience.endDate}
-        ></Input>
-      </div>,
+      <NewWorkExperience
+        key={experience.id}
+        id={experience.id}
+        title={experience.title}
+        companyName={experience.companyName}
+        description={experience.description}
+        startDate={experience.startDate}
+        endDate={experience.endDate}
+      ></NewWorkExperience>,
     );
   });
 

61a624b722683cf2012402c03b6da577cf9a6fb7
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-10 12:17:30 +0530

    Add styling to CVInformation component
    Give cv-information class to form element

--- src/components/cv-information/cv-information.jsx
+++ src/components/cv-information/cv-information.jsx
@@ -1,10 +1,11 @@
 import { GeneralInformation } from "./general-information/general-information";
 import { Education } from "./education/education";
 import { WorkExperience } from "./work-experience/work-experience";
+import "./cv-information.css";
 
 export function CVInformation({ cvInformation, updateCvInformation }) {
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form onSubmit={(e) => e.preventDefault()} className="cv-information">
       <GeneralInformation
         generalInformation={cvInformation.generalInformation}
         updateGeneralInformation={updateCvInformation}

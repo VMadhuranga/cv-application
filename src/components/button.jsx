27b5927190a5771747e9ad7921029b10102602aa
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-10 12:16:50 +0530

    Add Button component styling
    Create className prop to give different style classes to button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,11 +1,17 @@
-export function Button({ text, handleOnClick, disabled }) {
+import "./button.css";
+
+export function Button({ text, handleOnClick, disabled, className }) {
   if (disabled) {
     return (
-      <button onClick={handleOnClick} disabled hidden>
+      <button onClick={handleOnClick} className={className} disabled hidden>
         {text}
       </button>
     );
   }
 
-  return <button onClick={handleOnClick}>{text}</button>;
+  return (
+    <button onClick={handleOnClick} className={className}>
+      {text}
+    </button>
+  );
 }
